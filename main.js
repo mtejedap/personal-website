49ae67124771e1800bf13e2ed737c0460950b0ac
Matias Tejeda Astaburuaga <54756345+mtejedap@users.noreply.github.com>
2024-05-27 17:27:12 -0400

    Update bio

--- main.js
+++ main.js
@@ -33,10 +33,10 @@ english.addEventListener("click", () => {
     const aboutBody = document.querySelector(".about-body");
     aboutBody.style.opacity = "0";
     setTimeout(() => {
-        aboutBody.textContent = "Hi, I'm Matias! I'm a Software Engineer with a Bachelor's degree in Computer Science from Purdue University. 
-            I love playing around with computers and finding creative solutions to challenging problems. My areas of expertise include full-stack applications, 
-            operating systems, computer networks, and embedded systems. In my spare time, I like to work on personal projects or research interesting technologies 
-            to expand my skills. If you'd like to check out my work or talk to me, please click the icons above!";
+        aboutBody.textContent = "Hi, I'm Matias! I'm a Software Engineer with a Bachelor's degree in Computer Science from Purdue University." +
+            "I love playing around with computers and finding creative solutions to challenging problems. My areas of expertise include full-stack applications," +
+            "operating systems, computer networks, and embedded systems. In my spare time, I like to work on personal projects or research interesting technologies" +
+            "to expand my skills. If you'd like to check out my work or talk to me, please click the icons above!";
         aboutBody.style.opacity = "1";
     }, 500);
 
@@ -87,11 +87,11 @@ spanish.addEventListener("click", () => {
     const aboutBody = document.querySelector(".about-body");
     aboutBody.style.opacity = "0";
     setTimeout(() => {
-        aboutBody.textContent = "¡Hola, soy Matías! Soy un ingeniero de software con una licenciatura en Ciencias de Computación de Purdue University. 
-            Me encanta jugar con las computadoras y encontrar soluciones creativas a problemas desafiantes. Mis áreas de especialización 
-            incluyen aplicaciones full-stack, sistemas operativos, redes informáticas y sistemas integrados. En mi tiempo libre, me gusta 
-            trabajar en proyectos personales o investigar tecnologías interesantes para ampliar mis habilidades. Si desea ver mi trabajo 
-            o hablar conmigo, ¡haga click en los íconos de arriba!";
+        aboutBody.textContent = "¡Hola, soy Matías! Soy un ingeniero de software con una licenciatura en Ciencias de Computación de Purdue University." +
+            "Me encanta jugar con las computadoras y encontrar soluciones creativas a problemas desafiantes. Mis áreas de especialización" +
+            "incluyen aplicaciones full-stack, sistemas operativos, redes informáticas y sistemas integrados. En mi tiempo libre, me gusta" +
+            "trabajar en proyectos personales o investigar tecnologías interesantes para ampliar mis habilidades. Si desea ver mi trabajo" +
+            "o hablar conmigo, ¡haga click en los íconos de arriba!";
         aboutBody.style.opacity = "1";
     }, 500);
 
@@ -142,11 +142,11 @@ japanese.addEventListener("click", () => {
     const aboutBody = document.querySelector(".about-body");
     aboutBody.style.opacity = "0";
     setTimeout(() => {
-        aboutBody.textContent = "こんにちは、マティアスです。私は、パデュー大学でコンピューターサイエンスの学士号を取得したソフトウェアエン
-            ジニアです。コンピューターをいじったり、難しい問題に独創的な解決策を見つけたりするのが大好きです。私の専門分野は、フルスタックアプ
-            リケーション、オペレーティングシステム、コンピューターネットワーク、組み込みシステムです。余暇には、個人的なプロジェクトに取り組ん
-            だり、興味深いテクノロジーを研究してスキルを伸ばすのが好きです。私の作品をご覧になりたい場合や、私と話したい場合は、上のアイコ
-            ンをクリックしてください。";
+        aboutBody.textContent = "こんにちは、マティアスです。私は、パデュー大学でコンピューターサイエンスの学士号を取得したソフトウェアエン" +
+            "ジニアです。コンピューターをいじったり、難しい問題に独創的な解決策を見つけたりするのが大好きです。私の専門分野は、フルスタックアプ" +
+            "リケーション、オペレーティングシステム、コンピューターネットワーク、組み込みシステムです。余暇には、個人的なプロジェクトに取り組ん" +
+            "だり、興味深いテクノロジーを研究してスキルを伸ばすのが好きです。私の作品をご覧になりたい場合や、私と話したい場合は、上のアイコ" +
+            "ンをクリックしてください。";
         aboutBody.style.opacity = "1";
     }, 500);
 
